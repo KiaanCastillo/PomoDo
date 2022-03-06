4811428eed4a4acc1ec9b987a61973bac79aae5f
Kiaan Castillo <kiaan_castillo@sfu.ca>
2022-03-05 18:45:54 -0800

    Integrated Firebase, create new user and uid on first open

--- app/src/main/java/com/example/pomodo/MainActivity.kt
+++ app/src/main/java/com/example/pomodo/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.pomodo
 
-import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
@@ -10,14 +11,63 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
+
+data class Todo(
+    val name: String,
+    val duration: Int? = null,
+    val date: Date? = null,
+    val isChecked: Boolean,
+//    val createdAt: Date,
+//    val updatedAt: Date
+)
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var database: DatabaseReference
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var uid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = Firebase.database.reference
+        sharedPreferences = this?.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        initUser()
     }
+
+    private fun initUser() {
+        if (sharedPreferences.contains(getString(R.string.shared_preferences_uid_key))) {
+            uid = sharedPreferences.getString(getString(R.string.shared_preferences_uid_key), "")!!
+        } else {
+            createUser()
+        }
+        Toast.makeText(this, "My UID: $uid", Toast.LENGTH_LONG).show()
+    }
+
+    private fun createUser() {
+        with (sharedPreferences.edit()) {
+            val newUid = database.child(getString(R.string.database_users_collection_key)).push().key
+            putString(getString(R.string.shared_preferences_uid_key), newUid)
+            apply()
+            uid = newUid!!
+            database.child(getString(R.string.database_users_collection_key)).child(uid).child("uid").setValue(uid)
+        }
+    }
+
+//    fun addNewTodo(name: String,
+//                   duration: Int? = null,
+//                   date: Date? = null,
+//                   isChecked: Boolean,
+//                   createdAt: Date,
+//                   updatedAt: Date) {
+//        val newTodo = Todo(name, duration, date, isChecked)
+//        database.child("users").child(userId).setValue(user)
+//    }
 
     fun showAddNewTodoDialog(view: View) {
         val addNewTodoDialog: Dialog = Dialog(this)
